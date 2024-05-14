a2f09c5d574b387533fd54da5631055be5bd5ed7
JinJaffix1 <teagrenee@gmail.com>
2024-05-14 12:35:53 +1000

    Add files via upload

---
+++ Edge.java
@@ -0,0 +1,16 @@
+
+public class Edge
+{
+	protected Node friend;
+	
+	public Edge(Node friend)
+	{
+		this.friend = friend;
+	}
+	
+	public String toString(){
+		String str ="friend" + friend.toString();
+		return str;
+	}
+	
+}
