fd7e220c37658d97ade3bdbdd69c18980de9e1a6
JinJaffix1 <teagrenee@gmail.com>
2024-05-14 12:35:05 +1000

    Finished Node class

--- Node.java
+++ Node.java
@@ -1,4 +1,3 @@
-
 import java.time.LocalDate;
 import java.util.HashMap;
 
@@ -8,56 +7,64 @@ import java.util.HashMap;
  * @version April 2024
  * @author Saber Elsayed
  */
-class Node implements NodeInteface {
+class Node implements NodeInteface
+{
 
-    //id
-    private Integer id;
-    //person name
-    private String name;
+	// id
+	private Integer id;
+	
+	// person name
+	private String name;
 
-    //date of birth
-    private LocalDate dateOB;
-    //suburb a person lives at
-    private String suburb;
+	// date of birth
+	private LocalDate dateOB;
+	
+	// suburb a person lives at
+	private String suburb;
 
-    //contains a list of all firends of a person object
-    protected HashMap<Integer, Edge> adj;
+	// contains a list of all friends of a person object
+	protected HashMap<Integer, Edge> adj;
 
-    /**
-     * Construct a new vertex in the graph with the supplied id, name, DOB and
-     * suburb.
-     *
-     */
-    public Node(Integer id, String name, LocalDate dob, String suburb) {
-
-    }
+	/**
+	 * Construct a new vertex in the graph with the supplied id, name, DOB and suburb.
+	 */
+	public Node(Integer id, String name, LocalDate dob, String suburb)
+	{
+		this.id = id;
+		this.name = name;
+		this.dateOB = dob;
+		this.suburb = suburb;
+		this.adj = new HashMap<>();
+	}
 
 	@Override
 	public Integer getId()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return id;
 	}
 
 	@Override
 	public String getName()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
 	@Override
 	public String getSuburb()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return suburb;
 	}
 
 	@Override
 	public LocalDate getDateOB()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return dateOB;
+	}
+
+	public String toString()
+	{
+		String str = "id = " + getId() + "\nname = " + getName() + "\nDOB = " + getDateOB() + "\nsuburb = " + getSuburb();
+		return str;
 	}
 
 }
