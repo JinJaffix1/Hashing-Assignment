be44c6a4bbfb7ed2a35b4fc5ffdadad4c1742276
JinJaffix1 <teagrenee@gmail.com>
2024-05-20 21:28:47 +1000

    Update Edge.java

--- Edge.java
+++ Edge.java
@@ -3,13 +3,17 @@ public class Edge
 {
 	protected Node friend;
 	
-	public Edge(Node friend) {
+	public Edge(Node friend)
+	{
 		this.friend = friend;
 	}
 	
-	public String toString() {
-		String str ="friend" + friend.toString();
+	public String toString(){
+		String str ="friend: " + friend.toString();
 		return str;
+	}
+	public Node getFriend() {
+		return friend;
 	}
 	
 }
